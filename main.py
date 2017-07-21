9f14f27020e0c6362b5bef3b2985e0bf0ee985e5
ctam91 <ctam91@gmail.com>
2017-07-20 23:34:33 -0700

    add time

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,39 +16,46 @@ class Blog(db.Model):
     title = db.Column(db.String(80))
     body = db.Column(db.Text)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    pub_date = db.Column(db.DateTime)
 
 #Creates title, body, and owner properties for Blog class. 
-    def __init__(self, title, body, owner):
+    def __init__(self, title, body, owner, pub_date=None):
         self.title = title
         self.body = body
         self.owner = owner
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
 
 #create a User class with id, username, password, and blogs columns. 'blogs' links to Blogs table. 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(20))
     blogs = db.relationship('Blog', backref='owner')
 
-#Creates email and password properties for User class. 
-    def __init__(self, email, password):
-        self.email = email
+#Creates username and password properties for User class. 
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
 
-#user is allowed to visit /login or /register if they are not logged on. If user wants to access a different site and are not logged in, then redirect them to login page ..
+#user is allowed to visit /login, /signup, /blog, and /index if they are not logged on. If user wants to access a different site and are not logged in, then redirect them to login page ..
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register', 'blog', 'index']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    allowed_routes = ['login', 'signup', 'blog', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
 #create /newpost route and renders add template. If user leaves title or body blank, then return errors. 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
+    '''
+    Creates a new blog post
+    '''
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        owner = User.query.filter_by(email=session['email']).first()
+        owner = User.query.filter_by(username=session['username']).first()
 
         if title == "":
             flash("Please fill in the title", "error")
@@ -56,10 +64,10 @@ def newpost():
             flash("Please fill in the body", "error")
             
         if len(title) > 1 and len(body) > 1:   
-            new_post = Blog(title, body, owner) #'owner', not 'owner_id" or 'user' since 'owner' is property of Blog class
-            db.session.add(new_post)
-            db.session.commit()
-            blog_id = str(new_post.id) #grab the id for the record you just created
+            new_post = Blog(title, body, owner) #create an instance of a Blog class called new_post
+            db.session.add(new_post) 
+            db.session.commit() 
+            blog_id = str(new_post.id) 
             return redirect('/blog?id='+blog_id)
         else: 
             return render_template('newpost.html', title=title, body=body)
@@ -69,54 +77,73 @@ def newpost():
 #create /blog route to display blog
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
-    #since this is a get request, use request.args.get to retrieve id of blog post and owner 
+    '''
+    Show all blog entries
+    '''
+    #retrieve id of blog post and owner from the url 
     blog_id = request.args.get('id')
     user_id = request.args.get('userId')
     
     #if blog_id, send your db a query and find the post associated with that id. Render post.html with that post's title and blog
     if blog_id:
-        post = Blog.query.filter_by(id=blog_id).first()
-        return render_template("post.html", title=post.title, body=post.body, user=post.owner.email)
+        post = Blog.query.filter_by(id=int(blog_id)).first()
+        return render_template("post.html", title=post.title, body=post.body, user=post.owner.username)
 
     #if user_id, find all Blog objects associated with that owner_id 
     if user_id:
         entries = Blog.query.filter_by(owner_id=int(user_id)).all()
         return render_template('user.html', entries=entries)
     
-# If there are no specific posts, show entire blog. 
+# If there are no specific posts to retrieve, show entire blog
     titles = Blog.query.all()
     return render_template('blog.html',titles=titles)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    # if post, means that user is trying to login and you need to verify email, password, and if user exists in database
+    '''
+    Shows login page and login form
+    '''
+    # if it is a post method, the user is trying to login and we need to verify email, password, and if user exists in database
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
+
         if user and user.password == password:
-            session['email'] = email    
+            session['username'] = username   
             flash("Logged in",'info')
             return redirect('/newpost')
+
+        if username == "" and password == "":
+            flash('Please enter a username and password','error')
+
         if user and user.password != password:
             flash('User password incorrect', 'error')
-        if not user:
+
+        if not user and len(password) > 1:
+            flash('Please enter a username', 'error')
+
+        if not user and len(username) > 1:
             flash('User does not exist', 'error')
-        return render_template('login.html', email=email)
+
+        return render_template('login.html', username=username)
 
     return render_template('login.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
-def register():
+def signup():
+    '''
+    Shows signup page and signup form
+    '''
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        existing_user = User.query.filter_by(email=email).first()
+        existing_user = User.query.filter_by(username=username).first()
 
-        if email == "" or len(email) < 3:
-            flash("Please provide a valid email", "error")
+        if username == "" or len(username) < 3:
+            flash("Please provide a valid username", "error")
 
         if password == "" or len(password) < 3:
             flash("Please provide a valid password. Password must be between 3-20 charcters", "error")
@@ -127,26 +154,32 @@ def register():
         if existing_user:
             flash("Duplicate username", 'error')
 
-        if len(email) > 3 and len(password) > 3 and password==verify and not existing_user: 
-            new_user = User(email, password)
+        if len(username) > 3 and len(password) > 3 and password==verify and not existing_user: 
+            new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            session['email'] = email
-            return redirect('/')
+            session['username'] = username
+            return redirect('/newpost')
         else:
-            return render_template('signup.html', email=email)
+            return render_template('signup.html', username=username)
 
     return render_template('signup.html')
 
 @app.route('/logout')
 def logout():
-    del session['email']
+    '''
+    Deletes session and logs user out
+    '''
+    del session['username']
     flash('logged out','info')
     return redirect('/')
 
 
 @app.route('/')
 def index():
+    '''
+    List all users
+    '''
     users = User.query.all()
     return render_template('index.html', users=users)
 
